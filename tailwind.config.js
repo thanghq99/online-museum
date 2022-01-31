f7a492595ee54234dcec0ab1752be787897d604b
thanghq99 <haikhauak@gmail.com>
2022-01-31 23:08:25 +0700

    install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./pages/index.js",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
